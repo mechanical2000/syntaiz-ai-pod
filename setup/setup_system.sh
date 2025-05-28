0cc210e6fa54bd9f8c504627913f786fd7a3dfab
Marc DEROYAN <mderoyan@MDEROYAN-MAC-25.local>
2025-05-28 18:05:34 +0200

    test

--- setup/setup_system.sh
+++ setup/setup_system.sh
@@ -21,12 +21,20 @@ apt update && apt install -y \
     nano \
     nginx
 
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+pyenv install 3.10.13
+pyenv global 3.10.13
+
 # 🔧 Python et pip
 pip install --upgrade pip
 pip install wheel setuptools
-pip install --no-cache-dir \
+pip install --prefer-binary --no-cache-dir \
     numpy \
     torch==2.2.0 --index-url https://download.pytorch.org/whl/cu118 \
+    bitsandbytes \
     transformers \
     accelerate \
     sentencepiece \
@@ -34,10 +42,7 @@ pip install --no-cache-dir \
     huggingface_hub \
     fastapi \
     uvicorn \
-    bitsandbytes \
     protobuf
-
-
 
 # 📥 Téléchargement conditionnel du modèle
 if [ ! -d "$MODEL_DIR" ]; then
